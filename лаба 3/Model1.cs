4af387d2d3d4288383864167cbe30bb6f85eed29
камиль исмаилов <sgfejs99@gmail.com>
2020-04-15 14:53:03 +0300

    Добавьте файлы проекта.

---
+++ лаба 3/Model1.cs
@@ -0,0 +1,22 @@
+﻿namespace лаба_3
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Логин")
+        {
+        }
+
+        public virtual DbSet<Логин> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
+        
